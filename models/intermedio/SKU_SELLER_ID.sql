6ff4250930589032a77025d881d9eef7363e3797
Benjamin Castaneda <benjamin@biqu.cl>
2024-08-02 03:21:40 +0000

    changes

--- models/intermedio/SKU_SELLER_ID.sql
+++ models/intermedio/SKU_SELLER_ID.sql
@@ -1,4 +1,20 @@
-SELECT SID.id as seller_id,SL.sku,SL.retail_id,seller_name FROM (select sku,retail_id,LOWER(seller) as seller_name 
-                                 from  {{ source('comercial','SELLERS')}}  group by sku,retail_id,LOWER(seller)) as SL 
-                                JOIN {{ source('comercial','SELLERS_ID')}}   SID
-                                  ON LOWER(SL.seller_name) = LOWER(SID.seller) and SL.retail_id =SID.retail_id
+SELECT 
+    SID.id AS seller_id,
+    SL.sku,
+    SL.retail_id,
+    SL.seller_name
+FROM (
+    SELECT 
+        sku,
+        retail_id,
+        LOWER(seller) AS seller_name
+    FROM 
+        {{ source('comercial', 'SELLERS') }}
+    GROUP BY 
+        sku, retail_id, LOWER(seller)
+) AS SL
+JOIN 
+    {{ source('comercial', 'SELLERS_ID') }} SID
+ON 
+    LOWER(SL.seller_name) = LOWER(SID.seller) 
+    AND SL.retail_id = SID.retail_id;
--- models/paris/PRODUCT_DETAILS_PARIS.sql
+++ models/paris/PRODUCT_DETAILS_PARIS.sql
@@ -1,12 +1,4 @@
- WITH SL AS (
 
-SELECT SID.id as seller_id,SL.sku,SL.retail_id,seller_name FROM (select sku,retail_id,LOWER(seller) as seller_name 
-                                 from  pasaporcaja.COMERCIAL.SELLERS  group by sku,retail_id,LOWER(seller)) as SL 
-                                JOIN pasaporcaja.COMERCIAL.SELLERS_ID  SID
-                                  ON LOWER(SL.seller_name) = LOWER(SID.seller) and SL.retail_id =SID.retail_id
-
- )
- 
  
  SELECT P.product_id,
                                  SL.seller_id,
