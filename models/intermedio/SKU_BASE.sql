941eb8ab163c3c1fca3ce73cd5062f71e7412ce8
Benjamin Castaneda <benjamin@biqu.cl>
2024-08-03 01:21:23 +0000

    sku_base

--- models/intermedio/SKU_BASE.sql
+++ models/intermedio/SKU_BASE.sql
@@ -4,7 +4,11 @@
     cluster_by = ["retail_id","sku"]
   )
 }}
-WITH CURRENT_DATA AS (SELECT scraped_at,scraped_at_date,sku,retail_id,product_name,product_url,offer_price,normal_price,card_price,loyalty_price,picture_url ,CASE WHEN brand is null THEN 'sin marca' ELSE lower(brand) END AS brand,rating,n_reviews,pos,  ROW_NUMBER() OVER (PARTITION BY sku,retail_id ORDER BY scraped_at DESC) as N
+WITH CURRENT_DATA AS (
+    SELECT scraped_at,scraped_at_date,sku,retail_id,product_name,product_url,offer_price,normal_price,card_price,loyalty_price,picture_url ,
+    CASE WHEN brand is null THEN 'sin marca' ELSE lower(brand) END AS brand,rating,n_reviews,pos, 
+     ROW_NUMBER() OVER (PARTITION BY sku,retail_id ORDER BY scraped_at DESC) as N,
+     CURRENT_DATE('America/Santiago') AS process_date
             FROM {{ source('operacion','SCRAPINGS')}} ),
             LAST_ROW AS ( SELECT *  EXCEPT(N)
             FROM CURRENT_DATA
